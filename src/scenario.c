23aeca1e4b8ac3289247e1c1c66356be306b3605
modsrm <modsrm@pagefault.io>
2015-02-01 01:13:16 +0000

    Fix axes direction's vertices initialization.

--- src/scenario.c
+++ src/scenario.c
@@ -81,26 +81,26 @@ static void set_axes_direction_vertices(double plane_length,
     int tri_side_px = 20;
     int half_tri_side_px = tri_side_px / 2;
 
-    x_axis_direction_vert[0].x = 0;
-    x_axis_direction_vert[0].y = half_x;
+    x_axis_direction_vert[0].x = half_x;
+    x_axis_direction_vert[0].y = 0;
 
-    x_axis_direction_vert[1].x = - half_tri_side_px;
-    x_axis_direction_vert[1].y = half_x
+    x_axis_direction_vert[1].x = half_x
         - get_tri_eq_height_from_side(tri_side_px);
 
-    x_axis_direction_vert[2].y = x_axis_direction_vert[1].y;
+    x_axis_direction_vert[1].y = - half_tri_side_px;
 
-    x_axis_direction_vert[2].x = half_tri_side_px;
+    x_axis_direction_vert[2].x = x_axis_direction_vert[1].x;
+    x_axis_direction_vert[2].y = half_tri_side_px;
 
-    y_axis_direction_vert[0].x = half_y;
-    y_axis_direction_vert[0].y = 0;
-    y_axis_direction_vert[1].y = - half_tri_side_px;
-    y_axis_direction_vert[1].x = half_y
+    y_axis_direction_vert[0].x = 0;
+    y_axis_direction_vert[0].y = half_y;
+
+    y_axis_direction_vert[1].x = - half_tri_side_px;
+    y_axis_direction_vert[1].y = half_y
         - get_tri_eq_height_from_side(tri_side_px);
 
-    y_axis_direction_vert[2].x = y_axis_direction_vert[1].x;
-
-    y_axis_direction_vert[2].y = half_tri_side_px;
+    y_axis_direction_vert[2].x = half_tri_side_px;
+    y_axis_direction_vert[2].y = y_axis_direction_vert[1].y;
 }
 
 void set_plane_size(double length, double height) {
