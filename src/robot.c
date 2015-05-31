7aa904af9f4390e868d96636c0e7d4542c2e10c5
modsrm <roberto@pagefault.io>
2015-05-31 20:10:48 +0100

    Fix #11: implement robot at destination mechanism.

--- src/robot.c
+++ src/robot.c
@@ -17,6 +17,13 @@ static struct
                   .position.y = 0.0,
                   .heading = 0.0
                 };
+static bool is_at_destination()
+{
+    double x_delta = robot_state.destination.x - robot_state.position.x;
+    double y_delta = robot_state.destination.y - robot_state.position.y;
+
+    return fabs(x_delta) < 15 && fabs(y_delta) < 15 ? true : false;
+}
 
 // This method updates the robot's position and heading at each time tick.
 void update_robot_state()
@@ -25,21 +32,21 @@ void update_robot_state()
     printf("%s: robot destination %f %f\n", __func__,
             robot_state.destination.x, robot_state.destination.y);
 
-    // TODO: this comparison is broken. See #8.
-    if(robot_state.position.x == robot_state.destination.x &&
-       robot_state.position.y == robot_state.destination.y)
+    if(is_at_destination())
     {
         return;
     }
 
-    struct Vertex new_pos = get_new_pos(robot_state.position, robot_state.heading);
+    struct Vertex new_pos = get_new_pos(robot_state.position,
+                                        robot_state.heading);
 
     robot_state.position.x = new_pos.x;
     robot_state.position.y = new_pos.y;
 
     printf("%s: new position x=%f y=%f\n", __func__, new_pos.x, new_pos.y);
 
-    robot_state.heading = get_new_angle(robot_state.position, robot_state.destination);
+    robot_state.heading = get_new_angle(robot_state.position,
+                                        robot_state.destination);
 
     printf("%s: new robot_state heading %f\n", __func__, robot_state.heading);
     printf("RSU END:\n\n");
