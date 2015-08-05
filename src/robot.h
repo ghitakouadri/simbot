f02fab0157c53dff9bb865526bec693f50cdc284
modsrm <roberto@pagefault.io>
2015-08-05 22:06:40 +0100

    Add doxygen doc to robot.h.

--- src/robot.h
+++ src/robot.h
@@ -3,9 +3,30 @@
 
 struct Vertex;
 
+/**
+ * Sets the robot destination to the provided location.
+ *
+ * @param[in] dest the vertex that identifies the destination.
+ */
 void set_robot_destination(struct Vertex dest);
+
+/**
+ * Updates the robot state for the preconfigured delta t.
+ */
 void update_robot_state(void);
+
+/**
+ * Gets the robot position.
+ *
+ * @return the current position of the robot.
+ */
 struct Vertex get_robot_position(void);
+
+/**
+ * Gets the robot heading.
+ *
+ * @return the current heading of the robot, in deg.
+ */
 double get_robot_heading(void);
 
 #endif // ~ROBOT_H
